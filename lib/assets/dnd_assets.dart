254ce03d0dc0e51364f795223d366a00969dfd08
Aidyl <jgarciaalbalah@gmail.com>
2022-01-07 16:08:59 -0500

    + DnDAssets class

---
+++ lib/assets/dnd_assets.dart
@@ -0,0 +1,9 @@
+class DnDAssets {
+  static const _assets = "packages/citmatel_strawberry_dnd/assets";
+  static const _images = "$_assets/images";
+
+  ////IMAGES////
+  static const String WALLPAPER = "$_images/test.jpg";
+  static const String LEVEL1TEST = "$_images/brain_in_blue.jpg";
+
+}
