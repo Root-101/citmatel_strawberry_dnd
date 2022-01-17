d943745486b06437b3f7ba1d6d0531fd3abcf560
JHB <jhernandezb96@gmail.com>
2022-01-16 23:43:08 -0500

    +repo/repo_external

---
+++ lib/src/repo/d_repo_external/dnd_level_progress_repo_external.dart
@@ -0,0 +1,5 @@
+import 'package:citmatel_strawberry_dnd/dnd_exporter.dart';
+import 'package:clean_repo_objectbox/clean_objectbox_exporter.dart';
+
+abstract class DnDLevelProgressRepoExternal
+    extends ObjectBoxCRUDRepositoryExternal<DnDLevelProgressEntity> {}
---
+++ lib/src/repo/d_repo_external/dnd_repo_external_exporter.dart
@@ -0,0 +1,5 @@
+library dnd_repo_external_exporter;
+
+export 'dnd_level_progress_repo_external.dart';
+//export the External repos
+export 'dnd_sublevel_progress_repo_external.dart';
