cab3bdd45e48a53dfde57dde97cc0569b2638656
JHB <jhernandezb96@gmail.com>
2022-01-26 23:58:24 -0500

    DnDLevelsScreen: +new Commons Level

--- lib/src/ui/f_widget/levels/dnd_levels_screen.dart
+++ lib/src/ui/f_widget/levels/dnd_levels_screen.dart
@@ -10,6 +10,41 @@ class DnDLevelsScreen extends GetView<DnDLevelController> {
 
   @override
   Widget build(BuildContext context) {
+    return GetBuilder<DnDLevelController>(builder: (context) {
+      return CommonsLevelsThemeScreen<DnDLevelDomain>(
+        levelsFindAll: controller.findAll(),
+        singleThemeTileBuilder: (levelDomain) {
+          return CommonsLevelsThemeSingleTile<DnDLevelDomain>(
+            singleLevelDomain: levelDomain,
+            buildThemeName: (levelDomain) => levelDomain.theme,
+            openWidget: CommonsSingleLevel<DnDSubLevelDomain>(
+              themeTitle: levelDomain.theme,
+              maxStars: 0,
+              winedStars: 0,
+              subLevelsAll: levelDomain.sublevel,
+              urlThemePicture: levelDomain.urlThemePicture,
+              singleSubLevelTileBuilder: (subLevelDomain) {
+                DnDSubLevelProgressDomain progressDomain =
+                    Get.find<DnDSubLevelProgressUseCase>().findByAll(
+                  levelDomain,
+                  subLevelDomain,
+                );
+                return CommonsSingleSubLevelTile(
+                  stars: progressDomain.stars,
+                  contPlayedTimes: progressDomain.contPlayedTimes,
+                  openWidget: DnDSubLevelLoading(
+                    subLevelDomain: subLevelDomain,
+                    subLevelProgressDomain: progressDomain,
+                  ),
+                );
+              },
+            ),
+          );
+        },
+        urlSliverBackground: DnDAssets.WALLPAPER,
+      );
+    });
+/*
     return CommonsLevelsScreen<DnDLevelDomain>(
       levelsFindAll: controller.findAll(),
       buildSingleLevel: (levelDomain) {
@@ -39,6 +74,6 @@ class DnDLevelsScreen extends GetView<DnDLevelController> {
           );
         });
       },
-    );
+    );*/
   }
 }
