f6bf0ced4fab4510d48b8426028d4b0d50deebd1
JHB <jhernandezb96@gmail.com>
2022-02-05 20:50:51 -0500

    Update stars when win/loose level

--- lib/src/ui/f_widget/levels/dnd_levels_screen.dart
+++ lib/src/ui/f_widget/levels/dnd_levels_screen.dart
@@ -40,61 +40,66 @@ class DnDLevelsScreen extends GetView<DnDLevelController> {
           //builder de cada tile, uno por tema/uno por nivel
           singleThemeTileBuilder: (levelDomain) {
             //single level/tema tile por defecto
-            return CommonsLevelsThemeSingleTile<DnDLevelDomain>(
-              //levelDomain para generar las cosas de aqui
-              singleLevelDomain: levelDomain,
-              //color primario, principalmente para animaciones
-              colorPrimary: levelDomain.themeBackgroundImage.colorStrong,
-              //tema del tile, generado a partir del `levelDomain`
-              buildThemeName: (levelDomain) => levelDomain.theme,
-              //foto del tema del tile, generado a partir del `levelDomain`
-              buildThemeUrlImage: (levelDomain) =>
-                  levelDomain.themeBackgroundImage.urlImage,
-              //nivel abierto, lista de subniveles
-              openWidget: CommonsSingleLevel<DnDSubLevelDomain>(
-                //level domain para random
-                levelDomain: levelDomain,
-                //funcion para generar un nivel random cada vez, recive por defecto el levelDomain
-                onRandomOfTap: controller.randomSubLevelOf,
-                //titulo del tema
-                themeTitle: levelDomain.theme,
-                //foto del tema, para mostrar en el sliver
-                urlThemePicture: levelDomain.themeBackgroundImage.urlImage,
-                //color fuerte relacionado con la imagen
+            return GetBuilder<DnDLevelController>(builder: (_) {
+              return CommonsLevelsThemeSingleTile<DnDLevelDomain>(
+                //levelDomain para generar las cosas de aqui
+                singleLevelDomain: levelDomain,
+                //color primario, principalmente para animaciones
                 colorPrimary: levelDomain.themeBackgroundImage.colorStrong,
-                //color debil relacionado con la imagen
-                colorSecondary: levelDomain.themeBackgroundImage.colorLight,
-                //estrellas maximas a ganar
-                maxStars: 0,
-                //estrellas ganadas
-                winedStars: 0,
-                //lista de los subniveles del tema
-                subLevelsAll: levelDomain.sublevel,
-                //builder de cada tile
-                singleSubLevelTileBuilder: (subLevelDomain) {
-                  //cargo el progreso de cada subnivel
-                  DnDSubLevelProgressDomain progressDomain =
-                      Get.find<DnDSubLevelProgressUseCase>().findByAll(
-                    levelDomain,
-                    subLevelDomain,
-                  );
-                  //tile generico
-                  return CommonsSingleSubLevelTile(
-                    //el primario de aqui es el secundario del otro lado
-                    colorPrimary: levelDomain.themeBackgroundImage.colorLight,
-                    //estrellas ganadas en el subnivel
-                    stars: progressDomain.stars,
-                    //cantidad de veces jugado el subnivel
-                    contPlayedTimes: progressDomain.contPlayedTimes,
-                    //nivel abierto, juego como tal
-                    openWidget: DnDSubLevelLoading(
-                      subLevelDomain: subLevelDomain,
-                      subLevelProgressDomain: progressDomain,
-                    ),
-                  );
-                },
-              ),
-            );
+                //tema del tile, generado a partir del `levelDomain`
+                buildThemeName: (levelDomain) => levelDomain.theme,
+                //foto del tema del tile, generado a partir del `levelDomain`
+                buildThemeUrlImage: (levelDomain) =>
+                    levelDomain.themeBackgroundImage.urlImage,
+                //nivel abierto, lista de subniveles
+                openWidget: CommonsSingleLevel<DnDSubLevelDomain>(
+                  //level domain para random
+                  levelDomain: levelDomain,
+                  //funcion para generar un nivel random cada vez, recive por defecto el levelDomain
+                  onRandomOfTap: controller.randomSubLevelOf,
+                  //titulo del tema
+                  themeTitle: levelDomain.theme,
+                  //foto del tema, para mostrar en el sliver
+                  urlThemePicture: levelDomain.themeBackgroundImage.urlImage,
+                  //color fuerte relacionado con la imagen
+                  colorPrimary: levelDomain.themeBackgroundImage.colorStrong,
+                  //color debil relacionado con la imagen
+                  colorSecondary: levelDomain.themeBackgroundImage.colorLight,
+                  //estrellas maximas a ganar
+                  maxStars: 0,
+                  //estrellas ganadas
+                  winedStars: 0,
+                  //lista de los subniveles del tema
+                  subLevelsAll: levelDomain.sublevel,
+                  //builder de cada tile
+                  singleSubLevelTileBuilder: (subLevelDomain) {
+                    //cargo el progreso de cada subnivel
+                    DnDSubLevelProgressDomain progressDomain =
+                        Get.find<DnDSubLevelProgressUseCase>().findByAll(
+                      levelDomain,
+                      subLevelDomain,
+                    );
+                    //tile generico
+                    return GetBuilder<DnDLevelController>(builder: (_) {
+                      return CommonsSingleSubLevelTile(
+                        //el primario de aqui es el secundario del otro lado
+                        colorPrimary:
+                            levelDomain.themeBackgroundImage.colorLight,
+                        //estrellas ganadas en el subnivel
+                        stars: progressDomain.stars,
+                        //cantidad de veces jugado el subnivel
+                        contPlayedTimes: progressDomain.contPlayedTimes,
+                        //nivel abierto, juego como tal
+                        openWidget: DnDSubLevelLoading(
+                          subLevelDomain: subLevelDomain,
+                          subLevelProgressDomain: progressDomain,
+                        ),
+                      );
+                    });
+                  },
+                ),
+              );
+            });
           },
         );
       },
