91e36c539a98f52e5f005ede906ce22a521ff93c
JHB <jhernandezb96@gmail.com>
2022-02-09 21:28:11 -0500

    +LV 2 Sciencie

--- lib/assets/levels/dnd_levels_assets.dart
+++ lib/assets/levels/dnd_levels_assets.dart
@@ -3,10 +3,20 @@ class DnDLevelsAssets {
 
   ////Ciencias////
   static const String THEME_CIENCIA = "$_assets/ciencia";
+
+  //LV1
   static const String L1 = "$THEME_CIENCIA/1";
   static const String L1_BG = "$L1/bg.png";
   static const String L1_ITEM_1 = "$L1/1.png";
   static const String L1_ITEM_2 = "$L1/2.png";
   static const String L1_ITEM_3 = "$L1/3.png";
   static const String L1_ITEM_4 = "$L1/4.png";
+
+  //LV2
+  static const String L2 = "$THEME_CIENCIA/2";
+  static const String L2_BG = "$L2/bg.png";
+  static const String L2_ITEM_1 = "$L1/5.png";
+  static const String L2_ITEM_2 = "$L1/6.png";
+  static const String L2_ITEM_3 = "$L1/7.png";
+  static const String L2_ITEM_4 = "$L1/8.png";
 }
