fef14a5ac9f141a019e625b3ed3bba2b76939729
JHB <jhernandezb96@gmail.com>
2022-02-11 01:15:15 -0500

    theme: + arte & + geografia

---
+++ lib/src/app/b_domain/levels/dnd_level_arte.dart
@@ -0,0 +1,11 @@
+import 'package:citmatel_strawberry_dnd/dnd_exporter.dart';
+import 'package:citmatel_strawberry_tools/assets/assets_exporter.dart';
+
+class DnDLevelArte {
+  static final DnDLevelDomain levelArte = DnDLevelDomain(
+    id: 1,
+    theme: "Arte",
+    themeBackgroundImage: ToolsThemesAssets.THEME_ARTE_BACKGROUND,
+    sublevel: [],
+  );
+}
--- lib/src/app/b_domain/levels/dnd_level_exporter.dart
+++ lib/src/app/b_domain/levels/dnd_level_exporter.dart
@@ -1,6 +1,7 @@
 library dnd_levels_exporter;
 
-export 'dnd_level1.dart';
+export 'dnd_level_arte.dart';
+export 'dnd_level_geografia.dart';
 export 'dnd_level_science.dart';
 //exporta los levels
 export 'dnd_level_tutorial.dart';
