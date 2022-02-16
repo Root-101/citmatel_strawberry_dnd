07bddcb7a6f65a9cbb96ed2985ac02a5c81f009b
JHB <jhernandezb96@gmail.com>
2022-02-15 21:24:45 -0500

    General LV 1 data

--- lib/src/app/b_domain/levels/dnd_level_1_general.dart
+++ lib/src/app/b_domain/levels/dnd_level_1_general.dart
@@ -6,6 +6,39 @@ class DnDLevelGeneral {
     id: 1,
     theme: ToolsThemesAssets.THEME_GENERAL_NAME,
     themeBackgroundImage: ToolsThemesAssets.THEME_GENERAL_BACKGROUND,
-    sublevel: [],
+    sublevel: [
+      DnDSubLevelDomain(
+        id: 1,
+        urlImage: DnDLevelsAssets.GENERAL_L1_BG,
+        rows: 1,
+        columns: 6,
+        items: [
+          DnDSubLevelItemDomain.singlePosition(
+            id: 1,
+            urlImage: DnDLevelsAssets.GENERAL_L1_ITEM_1,
+            rowPosition: 0,
+            columnPosition: 1,
+          ),
+          DnDSubLevelItemDomain.singlePosition(
+            id: 2,
+            urlImage: DnDLevelsAssets.GENERAL_L1_ITEM_2,
+            rowPosition: 0,
+            columnPosition: 2,
+          ),
+          DnDSubLevelItemDomain.singlePosition(
+            id: 3,
+            urlImage: DnDLevelsAssets.GENERAL_L1_ITEM_3,
+            rowPosition: 0,
+            columnPosition: 3,
+          ),
+          DnDSubLevelItemDomain.singlePosition(
+            id: 4,
+            urlImage: DnDLevelsAssets.GENERAL_L1_ITEM_4,
+            rowPosition: 0,
+            columnPosition: 4,
+          ),
+        ],
+      ),
+    ],
   );
 }
