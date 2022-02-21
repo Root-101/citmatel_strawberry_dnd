6ed1c37a76b9032153c33c2d139fdc7131af2a9c
JHB <jhernandezb96@gmail.com>
2022-02-21 00:12:24 -0500

    Level Animales: Fix ID

--- lib/src/app/b_domain/levels/dnd_level_7_animales.dart
+++ lib/src/app/b_domain/levels/dnd_level_7_animales.dart
@@ -3,7 +3,7 @@ import 'package:citmatel_strawberry_tools/tools_exporter.dart';
 
 class DnDLevelAnimales {
   static final DnDLevelDomain levelAnimales = DnDLevelDomain(
-    id: 9,
+    id: 7,
     theme: ToolsThemesAssets.THEME_ANIMALES_NAME,
     themeBackgroundImage: ToolsThemesAssets.THEME_ANIMALES_BACKGROUND,
     sublevel: [
--- lib/src/app/b_domain/levels/dnd_level_exporter.dart
+++ lib/src/app/b_domain/levels/dnd_level_exporter.dart
@@ -8,5 +8,5 @@ export 'dnd_level_4_mitologia.dart';
 //exporta los levels
 export 'dnd_level_5_deporte.dart';
 export 'dnd_level_6_geografia.dart';
-export 'dnd_level_9_animales.dart';
+export 'dnd_level_7_animales.dart';
 export 'dnd_levels_all.dart';
