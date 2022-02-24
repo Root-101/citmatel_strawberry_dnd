a80906493a6ece619ef9933e2a4f2ef44eb7a5d4
JHB <jhernandezb96@gmail.com>
2022-02-24 16:30:07 -0500

    Geografia: +LV1

--- lib/src/app/b_domain/levels/dnd_level_6_geografia.dart
+++ lib/src/app/b_domain/levels/dnd_level_6_geografia.dart
@@ -10,68 +10,44 @@ class DnDLevelGeografia {
       DnDSubLevelDomain(
         id: 1,
         urlImage: DnDLevelsAssets.GEOGRAFIA_L1_BG,
-        rows: 6,
-        columns: 6,
+        rows: 3,
+        columns: 3,
         items: [
           DnDSubLevelItemDomain.singlePosition(
             id: 1,
             urlImage: DnDLevelsAssets.GEOGRAFIA_L1_ITEM_1,
-            rowPosition: 4,
-            columnPosition: 4,
+            rowPosition: 0,
+            columnPosition: 0,
           ),
-          DnDSubLevelItemDomain(
+          DnDSubLevelItemDomain.singlePosition(
             id: 2,
             urlImage: DnDLevelsAssets.GEOGRAFIA_L1_ITEM_2,
-            possiblesPositions: [
-              DnDPositionDomain(
-                id: 1,
-                row: 2,
-                column: 2,
-              ),
-              DnDPositionDomain(
-                id: 2,
-                row: 3,
-                column: 2,
-              ),
-            ],
+            rowPosition: 0,
+            columnPosition: 1,
           ),
-          DnDSubLevelItemDomain(
+          DnDSubLevelItemDomain.singlePosition(
             id: 3,
             urlImage: DnDLevelsAssets.GEOGRAFIA_L1_ITEM_3,
-            possiblesPositions: [
-              DnDPositionDomain(
-                id: 1,
-                row: 0,
-                column: 1,
-              ),
-              DnDPositionDomain(
-                id: 2,
-                row: 1,
-                column: 1,
-              ),
-            ],
+            rowPosition: 0,
+            columnPosition: 2,
           ),
-          DnDSubLevelItemDomain(
+          DnDSubLevelItemDomain.singlePosition(
             id: 4,
             urlImage: DnDLevelsAssets.GEOGRAFIA_L1_ITEM_4,
-            possiblesPositions: [
-              DnDPositionDomain(
-                id: 1,
-                row: 2,
-                column: 3,
-              ),
-              DnDPositionDomain(
-                id: 2,
-                row: 1,
-                column: 3,
-              ),
-            ],
+            rowPosition: 2,
+            columnPosition: 0,
           ),
           DnDSubLevelItemDomain.singlePosition(
             id: 5,
             urlImage: DnDLevelsAssets.GEOGRAFIA_L1_ITEM_5,
-            rowPosition: 4,
+            rowPosition: 2,
             columnPosition: 1,
+          ),
+          DnDSubLevelItemDomain.singlePosition(
+            id: 6,
+            urlImage: DnDLevelsAssets.GEOGRAFIA_L1_ITEM_6,
+            rowPosition: 2,
+            columnPosition: 2,
           ),
         ],
       ),
@@ -316,6 +292,74 @@ class DnDLevelGeografia {
             urlImage: DnDLevelsAssets.GEOGRAFIA_L6_ITEM_6,
             rowPosition: 3,
             columnPosition: 2,
+          ),
+        ],
+      ),
+      DnDSubLevelDomain(
+        id: 7,
+        urlImage: DnDLevelsAssets.GEOGRAFIA_L7_BG,
+        rows: 6,
+        columns: 6,
+        items: [
+          DnDSubLevelItemDomain.singlePosition(
+            id: 1,
+            urlImage: DnDLevelsAssets.GEOGRAFIA_L7_ITEM_1,
+            rowPosition: 4,
+            columnPosition: 4,
+          ),
+          DnDSubLevelItemDomain(
+            id: 2,
+            urlImage: DnDLevelsAssets.GEOGRAFIA_L7_ITEM_2,
+            possiblesPositions: [
+              DnDPositionDomain(
+                id: 1,
+                row: 2,
+                column: 2,
+              ),
+              DnDPositionDomain(
+                id: 2,
+                row: 3,
+                column: 2,
+              ),
+            ],
+          ),
+          DnDSubLevelItemDomain(
+            id: 3,
+            urlImage: DnDLevelsAssets.GEOGRAFIA_L7_ITEM_3,
+            possiblesPositions: [
+              DnDPositionDomain(
+                id: 1,
+                row: 0,
+                column: 1,
+              ),
+              DnDPositionDomain(
+                id: 2,
+                row: 1,
+                column: 1,
+              ),
+            ],
+          ),
+          DnDSubLevelItemDomain(
+            id: 4,
+            urlImage: DnDLevelsAssets.GEOGRAFIA_L7_ITEM_4,
+            possiblesPositions: [
+              DnDPositionDomain(
+                id: 1,
+                row: 2,
+                column: 3,
+              ),
+              DnDPositionDomain(
+                id: 2,
+                row: 1,
+                column: 3,
+              ),
+            ],
+          ),
+          DnDSubLevelItemDomain.singlePosition(
+            id: 5,
+            urlImage: DnDLevelsAssets.GEOGRAFIA_L7_ITEM_5,
+            rowPosition: 4,
+            columnPosition: 1,
           ),
         ],
       ),
