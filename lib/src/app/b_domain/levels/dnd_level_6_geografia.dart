801c7d1e6e7ff98e33bdf2e636fabb1db7545c19
JHB <jhernandezb96@gmail.com>
2022-02-24 19:05:48 -0500

    Hint: geografia hasta 2

--- lib/src/app/b_domain/levels/dnd_level_6_geografia.dart
+++ lib/src/app/b_domain/levels/dnd_level_6_geografia.dart
@@ -18,36 +18,44 @@ class DnDLevelGeografia {
             urlImage: DnDLevelsAssets.GEOGRAFIA_L1_ITEM_1,
             rowPosition: 0,
             columnPosition: 0,
+            hint: "Pinar del Río, casa del mejor tabaco del mundo.",
           ),
           DnDSubLevelItemDomain.singlePosition(
             id: 2,
             urlImage: DnDLevelsAssets.GEOGRAFIA_L1_ITEM_2,
             rowPosition: 0,
             columnPosition: 1,
+            hint:
+            "El monumeto José Martí tiene casi 142 metros de altura y 78,50 metros de diámetro.",
           ),
           DnDSubLevelItemDomain.singlePosition(
             id: 3,
             urlImage: DnDLevelsAssets.GEOGRAFIA_L1_ITEM_3,
             rowPosition: 0,
             columnPosition: 2,
+            hint:
+            hint: "Holguín es conocida como la ciudad de los parques.",
           ),
           DnDSubLevelItemDomain.singlePosition(
             id: 4,
             urlImage: DnDLevelsAssets.GEOGRAFIA_L1_ITEM_4,
             rowPosition: 2,
             columnPosition: 0,
+            hint: "Matanzas es conocida como la ciudad de los puentes.",
           ),
           DnDSubLevelItemDomain.singlePosition(
             id: 5,
             urlImage: DnDLevelsAssets.GEOGRAFIA_L1_ITEM_5,
             rowPosition: 2,
             columnPosition: 1,
+            hint: "Camagüey es conocida como la ciudad de los tinajones.",
           ),
           DnDSubLevelItemDomain.singlePosition(
             id: 6,
             urlImage: DnDLevelsAssets.GEOGRAFIA_L1_ITEM_6,
             rowPosition: 2,
             columnPosition: 2,
+            hint: "La figura del mayor general Antonio Maceo tiene 16 m de altura y una colección de 23 \'machetes\' como símbolo del espíritu independiente de Cuba.",
           ),
         ],
       ),
@@ -62,10 +70,12 @@ class DnDLevelGeografia {
             urlImage: DnDLevelsAssets.GEOGRAFIA_L2_ITEM_1,
             rowPosition: 4,
             columnPosition: 4,
+            hint: "La Torre de Pisa es la torre campanario de la catedral de Pisa, situada en la plaza del Duomo de Pisa en Tuscana.",
           ),
           DnDSubLevelItemDomain(
             id: 2,
             urlImage: DnDLevelsAssets.GEOGRAFIA_L2_ITEM_2,
+            hint: "El Arco de Triunfo fue construido entre 1806 y 1836 por orden de Napoleón Bonaparte para conmemorar la victoria en la batalla de Austerlitz.",
             possiblesPositions: [
               DnDPositionDomain(
                 id: 1,
@@ -82,6 +92,7 @@ class DnDLevelGeografia {
           DnDSubLevelItemDomain(
             id: 3,
             urlImage: DnDLevelsAssets.GEOGRAFIA_L2_ITEM_3,
+            hint: "El Puente de la Torre es un puente basculante y colgante de Londres que cruza el río Támesis.",
             possiblesPositions: [
               DnDPositionDomain(
                 id: 1,
@@ -98,6 +109,7 @@ class DnDLevelGeografia {
           DnDSubLevelItemDomain(
             id: 4,
             urlImage: DnDLevelsAssets.GEOGRAFIA_L2_ITEM_4,
+            hint: "El Castillo de Neuschwanstein es un impresionante palacio neorrománico del siglo XIX, es considerado uno de los castillos más bonitos de alemania.",
             possiblesPositions: [
               DnDPositionDomain(
                 id: 1,
@@ -116,6 +128,7 @@ class DnDLevelGeografia {
             urlImage: DnDLevelsAssets.GEOGRAFIA_L2_ITEM_5,
             rowPosition: 4,
             columnPosition: 1,
+            hint: "La corrida de toros es seguramente una de las costumbres populares españolas más conocidas aunque al mismo tiempo más controvertidas.",
           ),
         ],
       ),
