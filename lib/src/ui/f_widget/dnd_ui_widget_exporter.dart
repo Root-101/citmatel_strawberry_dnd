42a10cda15d8ad36ae598477b131c692fe1a62ee
JHB <jhernandezb96@gmail.com>
2022-04-02 16:40:23 -0400

    Clean code

--- lib/src/ui/f_widget/dnd_ui_widget_exporter.dart
+++ lib/src/ui/f_widget/dnd_ui_widget_exporter.dart
@@ -1,7 +1,6 @@
 library dnd_ui_widget_exporter;
 
+export 'dnd_widget_constants.dart';
 //export all the widgets
 export 'levels/dnd_ui_levels_exporter.dart';
 export 'sublevel/dnd_ui_sublevel_exporter.dart';
-
-export 'dnd_widget_constants.dart';
