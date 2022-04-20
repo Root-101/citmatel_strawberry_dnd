701372dae611ba3ece195f8b61b448189d2fb5c6
JHB <jhernandezb96@gmail.com>
2022-04-20 16:58:03 -0400

    Only geografia

--- lib/src/app/b_domain/levels/dnd_level_6_geografia.dart
+++ lib/src/app/b_domain/levels/dnd_level_6_geografia.dart
@@ -3,7 +3,7 @@ import 'package:citmatel_strawberry_tools/assets/assets_exporter.dart';
 
 class DnDLevelGeografia {
   static final DnDLevelDomain levelGeografia = DnDLevelDomain(
-    id: 6,
+    id: 1,
     theme: ToolsThemesAssets.THEME_GEOGRAFIA_NAME,
     themeBackgroundImage: ToolsThemesAssets.THEME_GEOGRAFIA_BACKGROUND,
     sublevel: [
--- lib/src/app/b_domain/levels/dnd_levels_all.dart
+++ lib/src/app/b_domain/levels/dnd_levels_all.dart
@@ -3,12 +3,12 @@ import 'package:citmatel_strawberry_dnd/src/app/dnd_app_exporter.dart';
 class DnDLevelsAll {
   static final List<DnDLevelDomain> levels = [
     DnDLevelTutorial.tutorial,
-    DnDLevelGeneral.levelGeneral,
-    DnDLevelScience.levelCiencias,
-    DnDLevelTecnologia.levelTecnologia,
-    DnDLevelMitologia.levelMitologia,
-    DnDLevelDeporte.levelDeporte,
+    //DnDLevelGeneral.levelGeneral,
+    //DnDLevelScience.levelCiencias,
+    //DnDLevelTecnologia.levelTecnologia,
+    //DnDLevelMitologia.levelMitologia,
+    //DnDLevelDeporte.levelDeporte,
     DnDLevelGeografia.levelGeografia,
-    DnDLevelAnimales.levelAnimales,
+    //DnDLevelAnimales.levelAnimales,
   ];
 }
