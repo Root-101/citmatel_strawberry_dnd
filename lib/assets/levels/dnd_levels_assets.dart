23f48a9602888f5127da3427ea5921891df2df9e
JHB <jhernandezb96@gmail.com>
2022-07-12 11:24:58 -0400

    geografia: level 4 new

--- lib/assets/levels/dnd_levels_assets.dart
+++ lib/assets/levels/dnd_levels_assets.dart
@@ -18,8 +18,6 @@ class DnDLevelsAssets {
   static const String GEOGRAFIA_L1_ITEM_2 = "$GEOGRAFIA_L1/2.png";
   static const String GEOGRAFIA_L1_ITEM_3 = "$GEOGRAFIA_L1/3.png";
   static const String GEOGRAFIA_L1_ITEM_4 = "$GEOGRAFIA_L1/4.png";
-  static const String GEOGRAFIA_L1_ITEM_5 = "$GEOGRAFIA_L1/5.png";
-  static const String GEOGRAFIA_L1_ITEM_6 = "$GEOGRAFIA_L1/6.png";
 
   //LV2
   static const String GEOGRAFIA_L2 = "$THEME_GEOGRAFIA/2";
@@ -28,7 +26,6 @@ class DnDLevelsAssets {
   static const String GEOGRAFIA_L2_ITEM_2 = "$GEOGRAFIA_L2/2.png";
   static const String GEOGRAFIA_L2_ITEM_3 = "$GEOGRAFIA_L2/3.png";
   static const String GEOGRAFIA_L2_ITEM_4 = "$GEOGRAFIA_L2/4.png";
-  static const String GEOGRAFIA_L2_ITEM_5 = "$GEOGRAFIA_L2/5.png";
 
   //LV3
   static const String GEOGRAFIA_L3 = "$THEME_GEOGRAFIA/3";
@@ -37,8 +34,6 @@ class DnDLevelsAssets {
   static const String GEOGRAFIA_L3_ITEM_2 = "$GEOGRAFIA_L3/2.png";
   static const String GEOGRAFIA_L3_ITEM_3 = "$GEOGRAFIA_L3/3.png";
   static const String GEOGRAFIA_L3_ITEM_4 = "$GEOGRAFIA_L3/4.png";
-  static const String GEOGRAFIA_L3_ITEM_5 = "$GEOGRAFIA_L3/5.png";
-  static const String GEOGRAFIA_L3_ITEM_6 = "$GEOGRAFIA_L3/6.png";
 
   //LV4
   static const String GEOGRAFIA_L4 = "$THEME_GEOGRAFIA/4";
@@ -47,89 +42,4 @@ class DnDLevelsAssets {
   static const String GEOGRAFIA_L4_ITEM_2 = "$GEOGRAFIA_L4/2.png";
   static const String GEOGRAFIA_L4_ITEM_3 = "$GEOGRAFIA_L4/3.png";
   static const String GEOGRAFIA_L4_ITEM_4 = "$GEOGRAFIA_L4/4.png";
-  static const String GEOGRAFIA_L4_ITEM_5 = "$GEOGRAFIA_L4/5.png";
-  static const String GEOGRAFIA_L4_ITEM_6 = "$GEOGRAFIA_L4/6.png";
-
-  //LV5
-  static const String GEOGRAFIA_L5 = "$THEME_GEOGRAFIA/5";
-  static const String GEOGRAFIA_L5_BG = "$GEOGRAFIA_L5/bg.png";
-  static const String GEOGRAFIA_L5_ITEM_1 = "$GEOGRAFIA_L5/1.png";
-  static const String GEOGRAFIA_L5_ITEM_2 = "$GEOGRAFIA_L5/2.png";
-  static const String GEOGRAFIA_L5_ITEM_3 = "$GEOGRAFIA_L5/3.png";
-  static const String GEOGRAFIA_L5_ITEM_4 = "$GEOGRAFIA_L5/4.png";
-  static const String GEOGRAFIA_L5_ITEM_5 = "$GEOGRAFIA_L5/5.png";
-  static const String GEOGRAFIA_L5_ITEM_6 = "$GEOGRAFIA_L5/6.png";
-
-  //LV6
-  static const String GEOGRAFIA_L6 = "$THEME_GEOGRAFIA/6";
-  static const String GEOGRAFIA_L6_BG = "$GEOGRAFIA_L6/bg.png";
-  static const String GEOGRAFIA_L6_ITEM_1 = "$GEOGRAFIA_L6/1.png";
-  static const String GEOGRAFIA_L6_ITEM_2 = "$GEOGRAFIA_L6/2.png";
-  static const String GEOGRAFIA_L6_ITEM_3 = "$GEOGRAFIA_L6/3.png";
-  static const String GEOGRAFIA_L6_ITEM_4 = "$GEOGRAFIA_L6/4.png";
-  static const String GEOGRAFIA_L6_ITEM_5 = "$GEOGRAFIA_L6/5.png";
-  static const String GEOGRAFIA_L6_ITEM_6 = "$GEOGRAFIA_L6/6.png";
-
-  //LV7
-  static const String GEOGRAFIA_L7 = "$THEME_GEOGRAFIA/7";
-  static const String GEOGRAFIA_L7_BG = "$GEOGRAFIA_L7/bg.png";
-  static const String GEOGRAFIA_L7_ITEM_1 = "$GEOGRAFIA_L7/1.png";
-  static const String GEOGRAFIA_L7_ITEM_2 = "$GEOGRAFIA_L7/2.png";
-  static const String GEOGRAFIA_L7_ITEM_3 = "$GEOGRAFIA_L7/3.png";
-  static const String GEOGRAFIA_L7_ITEM_4 = "$GEOGRAFIA_L7/4.png";
-  static const String GEOGRAFIA_L7_ITEM_5 = "$GEOGRAFIA_L7/5.png";
-
-  //LV8
-  static const String GEOGRAFIA_L8 = "$THEME_GEOGRAFIA/8";
-  static const String GEOGRAFIA_L8_BG = "$GEOGRAFIA_L8/bg.png";
-  static const String GEOGRAFIA_L8_ITEM_1 = "$GEOGRAFIA_L8/1.png";
-  static const String GEOGRAFIA_L8_ITEM_2 = "$GEOGRAFIA_L8/2.png";
-  static const String GEOGRAFIA_L8_ITEM_3 = "$GEOGRAFIA_L8/3.png";
-  static const String GEOGRAFIA_L8_ITEM_4 = "$GEOGRAFIA_L8/4.png";
-  static const String GEOGRAFIA_L8_ITEM_5 = "$GEOGRAFIA_L8/5.png";
-  static const String GEOGRAFIA_L8_ITEM_6 = "$GEOGRAFIA_L8/6.png";
-
-  //LV9
-  static const String GEOGRAFIA_L9 = "$THEME_GEOGRAFIA/9";
-  static const String GEOGRAFIA_L9_BG = "$GEOGRAFIA_L9/bg.png";
-  static const String GEOGRAFIA_L9_ITEM_1 = "$GEOGRAFIA_L9/1.png";
-  static const String GEOGRAFIA_L9_ITEM_2 = "$GEOGRAFIA_L9/2.png";
-  static const String GEOGRAFIA_L9_ITEM_3 = "$GEOGRAFIA_L9/3.png";
-  static const String GEOGRAFIA_L9_ITEM_4 = "$GEOGRAFIA_L9/4.png";
-  static const String GEOGRAFIA_L9_ITEM_5 = "$GEOGRAFIA_L9/5.png";
-  static const String GEOGRAFIA_L9_ITEM_6 = "$GEOGRAFIA_L9/6.png";
-
-  //LV10
-  static const String GEOGRAFIA_L10 = "$THEME_GEOGRAFIA/10";
-  static const String GEOGRAFIA_L10_BG = "$GEOGRAFIA_L10/bg.png";
-  static const String GEOGRAFIA_L10_ITEM_1 = "$GEOGRAFIA_L10/1.png";
-  static const String GEOGRAFIA_L10_ITEM_2 = "$GEOGRAFIA_L10/2.png";
-  static const String GEOGRAFIA_L10_ITEM_3 = "$GEOGRAFIA_L10/3.png";
-  static const String GEOGRAFIA_L10_ITEM_4 = "$GEOGRAFIA_L10/4.png";
-  static const String GEOGRAFIA_L10_ITEM_5 = "$GEOGRAFIA_L10/5.png";
-  static const String GEOGRAFIA_L10_ITEM_6 = "$GEOGRAFIA_L10/6.png";
-
-  //LV11
-  static const String GEOGRAFIA_L11 = "$THEME_GEOGRAFIA/11";
-  static const String GEOGRAFIA_L11_BG = "$GEOGRAFIA_L11/bg.png";
-  static const String GEOGRAFIA_L11_ITEM_1 = "$GEOGRAFIA_L11/1.png";
-  static const String GEOGRAFIA_L11_ITEM_2 = "$GEOGRAFIA_L11/2.png";
-  static const String GEOGRAFIA_L11_ITEM_3 = "$GEOGRAFIA_L11/3.png";
-  static const String GEOGRAFIA_L11_ITEM_4 = "$GEOGRAFIA_L11/4.png";
-
-  //LV12
-  static const String GEOGRAFIA_L12 = "$THEME_GEOGRAFIA/12";
-  static const String GEOGRAFIA_L12_BG = "$GEOGRAFIA_L12/bg.png";
-  static const String GEOGRAFIA_L12_ITEM_1 = "$GEOGRAFIA_L12/1.png";
-  static const String GEOGRAFIA_L12_ITEM_2 = "$GEOGRAFIA_L12/2.png";
-  static const String GEOGRAFIA_L12_ITEM_3 = "$GEOGRAFIA_L12/3.png";
-  static const String GEOGRAFIA_L12_ITEM_4 = "$GEOGRAFIA_L12/4.png";
-
-  //LV13
-  static const String GEOGRAFIA_L13 = "$THEME_GEOGRAFIA/13";
-  static const String GEOGRAFIA_L13_BG = "$GEOGRAFIA_L13/bg.png";
-  static const String GEOGRAFIA_L13_ITEM_1 = "$GEOGRAFIA_L13/1.png";
-  static const String GEOGRAFIA_L13_ITEM_2 = "$GEOGRAFIA_L13/2.png";
-  static const String GEOGRAFIA_L13_ITEM_3 = "$GEOGRAFIA_L13/3.png";
-  static const String GEOGRAFIA_L13_ITEM_4 = "$GEOGRAFIA_L13/4.png";
 }
